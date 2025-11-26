9e63e77191127479901e7a433e26862bf01b970e
inhooo00 <chldlsgh0987@naver.com>
2025-11-26 16:33:46 +0900

    first commit

---
+++ src/main/java/urecagroup1backend/UrecaGroup1BackendApplication.java
@@ -0,0 +1,13 @@
+package urecagroup1backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrecaGroup1BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrecaGroup1BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/urecagroup1backend/UrecaGroup1BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package urecagroup1backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrecaGroup1BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
