facdc6b01c538e8f63d575fd600529d39cef422b
YeongHyeonHeo <djdjfug@naver.com>
2025-11-29 22:30:29 +0900

    [Fix] OrderResponse DTO(응답용) 수정

--- src/main/java/urecagroup1backend/orders/dto/OrderResponse.java
+++ src/main/java/urecagroup1backend/orders/dto/OrderResponse.java
@@ -6,7 +6,6 @@ import java.time.LocalDateTime;
 
 public record OrderResponse(
     Long orderId,
-    Long userId,
     String snackName,
     int snackPrice,
     String orderStatus,
@@ -16,8 +15,6 @@ public record OrderResponse(
     public static OrderResponse from(SnackOrder order){
         return new OrderResponse(
                 order.getId(),
-//                order.getUser().getId(),
-                null,
                 order.getSnack().getName(),
                 order.getSnack().getPrice(),
                 order.getStatus().name(),
