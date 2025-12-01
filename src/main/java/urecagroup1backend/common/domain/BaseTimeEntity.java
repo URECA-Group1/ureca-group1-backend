769eb6aadc125322d2f73800f26d6f88e444486c
HyeongseoShin <tlsgudtj123@gmail.com>
2025-12-01 10:32:58 +0900

    [Feat] BaseTimeEntity 추가 (#22)

---
+++ src/main/java/urecagroup1backend/common/domain/BaseTimeEntity.java
@@ -0,0 +1,18 @@
+package urecagroup1backend.common.domain;
+
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+// 여러 엔티티에서 사용될 생성시간, 업데이트 시간
+@MappedSuperclass // 모든 엔티티에서 이 클래스를 상속 받는 형식으로 (생성 시간, 업데이트 시간은 공통으로 필요한 것이라서)
+@Getter
+public class BaseTimeEntity {
+    @CreationTimestamp
+    private LocalDateTime createdTime;
+
+    @CreationTimestamp
+    private LocalDateTime updatedTime;
+}
---
+++ src/main/java/urecagroup1backend/seat_reservation/domain/SeatReservation.java
@@ -0,0 +1,4 @@
+package urecagroup1backend.seat_reservation.domain;
+
+public class SeatReservation {
+}
