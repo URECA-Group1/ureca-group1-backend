3e1faeacc77ba0ff5868d4513cd3512539769ec1
YeongHyeonHeo <djdjfug@naver.com>
2025-12-08 18:34:29 +0900

    [Feat] OrderRepository PAID 상태 필터링 조회 메서드 추가

--- src/main/java/urecagroup1backend/orders/Repository/OrderRepository.java
+++ src/main/java/urecagroup1backend/orders/Repository/OrderRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findAllByMemberId(Long memberId);
+    List<Order> findAllByMemberIdAndStatus(Long memberId, Order.OrderStatus status);
 }
