880be351bd9e924fd3b62701a52fb26b6f902aa4
HyeongseoShin <tlsgudtj123@gmail.com>
2025-12-13 12:00:51 +0900

    [Feat] 토큰 관련 커스텀 예외처리 추가 (#70)

--- src/main/java/urecagroup1backend/common/exception/ApiControllerAdvice.java
+++ src/main/java/urecagroup1backend/common/exception/ApiControllerAdvice.java
@@ -24,4 +24,13 @@ public class ApiControllerAdvice {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                 .body(new ApiResponse(HttpStatus.BAD_REQUEST, e.getMessage()));
     }
+
+    // 토큰 관련 예외 처리
+    @ExceptionHandler(InvalidTokenException.class)
+    public ResponseEntity<ApiResponse<?>> handleInvalidTokenException(InvalidTokenException e) {
+        log.warn("Invalid Token Exception: {}", e.getMessage());
+        // JWT/Refresh Token 문제(재로그인 필요)는 401 Unauthorized 또는 400 Bad Request
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED) // 💡 401 Unauthorized 권장
+                .body(new ApiResponse(HttpStatus.UNAUTHORIZED, e.getMessage()));
+    }
 }
